c89306438ebfef1bf02b2f025510960c8f701e84
Mariusz Świerczek <mariusz.swierczek@caops.pl>
2023-10-29 12:05:30 +0100

    Auction model and migration

---
+++ app/Models/Auction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Auction extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'insurance',
+        'end_date',
+        'year_of_prod',
+        'content'
+    ];
+}
